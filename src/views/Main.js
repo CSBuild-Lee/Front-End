bd55970deca8312d24280a8909c2abd217def91b
Louis-Charles Carrier <louis-charlescarrier@Louis-Charless-MacBook-Pro.local>
2020-02-03 13:17:41 -0500

    basic setup

---
+++ src/views/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    background: red;
+`
+
+const Main = () => {
+  return (
+    <Container>
+      
+    </Container>
+  );
+};
+
+export default Main;
