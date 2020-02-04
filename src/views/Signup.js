72a473d5e818663e78973042172149742266715c
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-02-04 13:44:41 -0500

    Sign-up form is done too.

--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,6 +1,65 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import bug from '../img/bug.png';
+
+const FormHeader = styled.h2`
+    text-align: center;
+    margin-top: 40px;
+    margin-bottom: -5px;
+`
+
+const FormContainer = styled.div`
+    height: 100vh;
+`
+
+const Form = styled.form`
+    display: flex;
+`
+
+const FormImg = styled.img`
+    height: 80vh;
+    margin-left: 40px;
+    margin-top: 40px;
+    margin-right: 160px;
+`
+
+const FlexContainer = styled.div`
+`
+
+const FormInputs = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+const InputLabel = styled.label`
+    margin-top: 40px;
+    margin-bottom: 10px;
+    text-align: center;
+`
+
+const TextInput = styled.input`
+    width: 25vw;
+    height: 5vh;
+    text-align: center;
+    color: #207653;
+    background: #FFEC00;
+    border: 1px solid #86ED00;
+`
+
+const SubmitButton = styled.button`
+    margin-top: 50px;
+    color: white;
+    background: #86ED00;
+    border: 1px solid #207653;
+    width: 25vw;
+    height: 5vh;
+`
+
+const FormDescription = styled.p`
+    text-align: center;
+    margin-top: 50px;
+`
 
 function Signup() {
     
@@ -22,31 +81,38 @@ function Signup() {
         
     return (
         <>
-            <div className="signup-container">
-                <form className="signup" onSubmit={handleSubmit}>
-                    <div className="inputs-container">
-                        <label>Username</label>
-                        <input 
-                            id="username"
-                            type="text"
-                            name="username"
-                            value={creds.username}
-                            placeholder="Enter your username."
-                            onChange={handleChange}
-                        />
-                        <label>Password</label>
-                        <input 
-                            id="password"
-                            type="password"
-                            name="password"
-                            value={creds.password}
-                            placeholder="Enter your password."
-                            onChange={handleChange}
-                        />
-                        <button type="submit">Create Account and Game Away</button>
-                    </div>
-                </form>
-            </div>
+            <FormContainer>
+                <Form onSubmit={handleSubmit}>
+                    <FlexContainer>
+                        <FormImg src={bug} />
+                    </FlexContainer>
+                    <FlexContainer>
+                        <FormInputs>
+                            <FormHeader>Angry Bugs</FormHeader>
+                            <InputLabel>Username</InputLabel>
+                            <TextInput 
+                                id="username"
+                                type="text"
+                                name="username"
+                                value={creds.username}
+                                placeholder="Enter your username."
+                                onChange={handleChange}
+                            />
+                            <InputLabel>Password</InputLabel>
+                            <TextInput 
+                                id="password"
+                                type="password"
+                                name="password"
+                                value={creds.password}
+                                placeholder="Enter your password."
+                                onChange={handleChange}
+                            />
+                            <SubmitButton type="submit">Click Here to Play</SubmitButton>
+                            <FormDescription>Create an account to save your best scores!</FormDescription>
+                        </FormInputs>
+                    </FlexContainer>
+                </Form>
+            </FormContainer>
         </>
     )
 }
